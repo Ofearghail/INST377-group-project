50efd9d2e3ebd96df23bf2bf82132bc6b329f278
Ofearghail <68789944+Ofearghail@users.noreply.github.com>
2024-12-14 14:19:51 -0500

    removed unused code

--- public/search.js
+++ public/search.js
@@ -1,36 +1,6 @@
 const citySearch = document.getElementById("search-bar");
 const dropdown = document.getElementById("search-dropdown");
 
-// // Fetch city data from GeoNames API
-// async function fetchCities(query) {
-//     if (!query) return [];
-    
-//     const username = 'ofearghail'; // Replace with your GeoNames username
-//     const apiUrl = `https://corsproxy.io/?key=852bd03c&url=http://api.geonames.org/searchJSON?q=${query}&maxRows=10&featureClass=P&username=${username}`;
-    
-//     try {
-//         const response = await fetch(apiUrl);
-//         const data = await response.json();
-        
-//         if (data.geonames) {
-
-//             console.log("Data:", data);
-//             return data.geonames.map(city => ({
-//                 name: `${city.name}, ${city.adminCode1}, ${city.countryCode}`,
-//                 lat: city.lat,
-//                 lng: city.lng
-//             }));
-//         } else {
-//             return [];
-//         }
-//     } catch (error) {
-//         console.error("Error fetching cities:", error);
-//         return [];
-//     }
-// }
-
-
-
 const API_KEY = "CS6Lxw9hGRYvRGbvsAl9knIO3SpHccIn";
 
 citySearch.addEventListener("input", async (e) => {
